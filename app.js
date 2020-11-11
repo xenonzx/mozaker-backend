caad710abdddec1b90192d4fc618e049b0bd4cfc
Ahmed EL-Dakhakhny <>
2020-11-11 14:49:31 +0200

    adding port from env vars

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+let port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(port,
+    () => console.log(`Server is running on port ${port}`));
+  
+  
