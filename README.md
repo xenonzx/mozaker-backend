f54e8ca084db84143b1f4ef046d0e9b3d14a1160
Ahmed EL-Dakhakhny <>
2020-11-11 17:18:41 +0200

    commit item with mongoose

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ TODO add admin panel
 DONE connect to heroku
 TODO connect to mongo atlas
 
+TODO serve welcome page at /
+TODO add version api to know db version
+TODO add api that return leatest version of collection
+
 
 #learning 
 Hapi
--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require('express')
+const AdminBro = require('admin-bro')
+const env = require('dotenv');
+const AdminBroExpress = require('@admin-bro/express')
+
+env.config();
 const app = express()
-let port = process.env.PORT || 3000
+const defaultPort = 3000
+let port = process.env.PORT || defaultPort
+
+const adminBro = new AdminBro ({
+    Databases: [],
+    rootPath: '/admin',
+})
+
+const router = AdminBroExpress.buildRouter (adminBro)
+app.use(adminBro.options.rootPath, router)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port,
-    () => console.log(`Server is running on port ${port}`));
+// todo  remove mongoose
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian2' });
+kitty.save().then(() => console.log('meow2'));
+
+app.listen(port,() => {
+    console.log(`Server is running on port ${port}`)
+    console.log(`AdminBro is under localhost:${port}/admin`)
+});
   
   
